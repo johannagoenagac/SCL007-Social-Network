730b72f2f47b432c7ecdc232fa192b53edbdf893
DaniAntu <d.asenjo.c@gmail.com>
2019-01-31 17:13:10 -0300

    Registro habilitado

--- src/main.js
+++ src/main.js
@@ -1,6 +1,33 @@
 //Manejo del DOM
+import {checkOutStatus} from '../src/data.js';
 
-window.onload =() =>{
+window.onload = () =>{
+
+checkAuthStatus((user)=>{
+    if(user){
+        //ocular o mostrar divs de login o de menu
+    }else{
+//ocular o mostrar divs de login o de menu
+    }
+});
+
+const registerWithGoogle = () =>{
+    registerUser();
+}
+
+googleregistry.addEventListener('click',registerWithGoogle);
+    
+
+  
+
+
+});
+
+
+
+
+
+
 
     
 }
