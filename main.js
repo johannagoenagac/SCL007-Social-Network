eda4f5f806d807c039f3d3557688e716a0a4f2d4
DaniAntu <d.asenjo.c@gmail.com>
2019-02-03 01:29:41 -0300

    Agrega función de logout para hacer pruebas, agrega span LOGOUT en index

--- main.js
+++ main.js
@@ -1,47 +1,81 @@
 //Manejo del DOM
 
-import {checkAuthStatus, registerUser} from '../src/data.js';
+import {checkAuthStatus, registerUser, logoutUser} from '../src/data.js';
 
 window.onload = () =>{
-
+    //Verifica estado de conexión del usuario
     checkAuthStatus((user)=>{
         if(user){
+            //Usuario logeado, muestra menú y home
+            header.style.display = "block"
             auth.style.display = "none";
             root.style.display = "block";
+            loginPage.style.display = "none";
+            post.style.display = "block";
         }else{
+            //Muestra el login, ya que usuario no está logeado
+            header.style.display = "none";
             auth.style.display = "block";
             root.style.display = "none";
+            footer.style.display = "none"
+            post.style.display = "none";
         }
     });
+    //Llama a la función registro con Google
     const registerWithGoogle = () =>{
         registerUser();
     }
+    const logoutUsers = () =>{
+        logoutUser();
+        
+    }
+
+    //Si hace click al botón Google, llama a la función registro con Google
     googleregistry.addEventListener('click',registerWithGoogle);
+    logout.addEventListener('click',logoutUsers);
+
+    function readFile(input) {
+        if (input.files && input.files[0]) {
+            var reader = new FileReader();
+ 
+            reader.onload = function (e) {
+                var filePreview = document.createElement('img');
+                filePreview.id = 'file-preview';
+                //e.target.result contents the base64 data from the image uploaded
+                filePreview.src = e.target.result;
+                console.log(e.target.result);
+ 
+                var previewZone = document.getElementById('file-preview-zone');
+                previewZone.appendChild(filePreview);
+            }
+ 
+            reader.readAsDataURL(input.files[0]);
+        }
+    }
+ 
+    var fileUpload = document.getElementById('file-upload');
+    fileUpload.onchange = function (e) {
+        readFile(e.srcElement);
+    }
+   
 };
 
-const home = document.getElementById("homeLogo");
-const search = document.getElementById("searchLogo");
-const add = document.getElementById("addLogo");
-const recipe = document.getElementById("recipeLogo");
-const user = document.getElementById("userLogo");
-const pageGuide = document.getElementById("pageGuide");
+homeLogo.addEventListener("click", () => {
+    pageGuide.innerHTML = "Home";
+});
 
-// home.addEventListener("click", () => {
-//     pageGuide.innerHTML = "Home";
-// });
+searchLogo.addEventListener("click", () => {
+    pageGuide.innerHTML = "Buscar";
+});
 
-// search.addEventListener("click", () => {
-//     pageGuide.innerHTML = "Buscar";
-// });
+addLogo.addEventListener("click", () => {
+    pageGuide.innerHTML = "Post";
+});
 
-// add.addEventListener("click", () => {
-//     pageGuide.innerHTML = "Post";
-// });
+recipeLogo.addEventListener("click", () => {
+    pageGuide.innerHTML = "Receta";
+});
 
-// recipe.addEventListener("click", () => {
-//     pageGuide.innerHTML = "Receta";
-// });
-
-// user.addEventListener("click", () => {
-//     pageGuide.innerHTML = "Perfil";
-// });
+userLogo.addEventListener("click", () => {
+    pageGuide.innerHTML = "Perfil";
+});
