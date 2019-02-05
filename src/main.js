af718f925c6b86b3e73395924be8bb3f1d07a90f
DaniAntu <d.asenjo.c@gmail.com>
2019-02-04 21:36:57 -0300

    Guarda cambios

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ window.onload = () =>{
             root.style.display = "block";
             loginPage.style.display = 'none';
             post.style.display = "block";
-            //muestra el nombre
+            //Muestra el nombre
             if(user !== null){
                 let name = user.displayName.split(" ");
                 document.getElementById('user-name').innerHTML = name[0];
