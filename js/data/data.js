c065607337e3fa46e242f761663c1ad5fd9f0e3a
VeronicaManchola <veronica.manchola@hotmail.com>
2019-02-07 18:00:26 -0300

    contador de likes funcional

--- js/data/data.js
+++ js/data/data.js
@@ -1,34 +1,34 @@
 
-export const addBiography = (textBiography,uid) => {
-  
+export const addBiography = (textBiography, uid) => {
+
 };
 
-export const savePost = (postImage, fullPostText, userID) => {  
+export const savePost = (postImage, fullPostText, userID) => {
   const newPostKey = firebase.database().ref('post').push().key;
 
 
   //sube información a firebase database
   firebase.database().ref(`timeline/${newPostKey}`).set({
-    image : postImage,
-    text : fullPostText,
-    useruid : userID,
-    likes : 0
+    image: postImage,
+    text: fullPostText,
+    useruid: userID,
+    likes: 0
   });
 
   firebase.database().ref(`profile/${userID}/post`).child(`${newPostKey}`).set({
 
 
-    image : postImage,
-    text : fullPostText,
-    likes : 0
+    image: postImage,
+    text: fullPostText,
+    likes: 0
   });
 };
 
 
 export const readPost = (onPostChange) => {
   let postRef = firebase.database().ref('timeline');
-  postRef.on('child_added', (post) => {
-    onPostChange(post);
+  postRef.on('value', (posts) => {
+    onPostChange(posts);
   });
 };
 
@@ -48,5 +48,3 @@ export const saveLikePost = (postID) => {
     }
   });
 }
-
-
