243da103784860f248f8e9df205d6747b3f16d7a
DaniAntu <d.asenjo.c@gmail.com>
2019-02-08 16:34:57 -0300

    Acepta cambios de merge - funcional en firefox

--- js/auth/auth.js
+++ js/auth/auth.js
@@ -70,6 +70,7 @@ export const registerUserGoogle = () => {
         // ...
         if(errorCode === 'auth/account-exists-with-different-credential'){
           alert('Usuario ya está registrado');
+
         }
      });
 };
